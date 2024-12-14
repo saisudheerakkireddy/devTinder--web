79430375fd32b1886c987d2fa16e9073813c58d3
Sudheerakkireddy <sudheerakkireddy@gmail.com>
2024-12-14 23:31:24 +0530

    Halt

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,44 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { addUser } from '../utils/userSlice';
-import { useNavigate } from 'react-router-dom';
-import { BASE_URL } from '../utils/constants';
 
+const EditProfile = () => {
 
-const Login = () => {
-    const [emailId, setEmailId] = useState('Elon@gmail.com');
-    const [password, setPassword]= useState('Elon@123');
-    const [error,setError] = useState("")
-    const dispatch = useDispatch(); 
-    const navigate = useNavigate();
-
-const handleLogin = async () => {
-
-  try{ 
-    
-    const res = await axios.post( BASE_URL + "/login", {
-
-        emailId,
-        password
-
-    },{withCredentials: true});
 
  
-    dispatch(addUser(res.data));
-    return navigate("/"); 
-    
-
-
-}catch(err){
-     setError(err?.response?.data)
-       
-    }
-
-};
-
-
   return (
     <div className='flex justify-center'>
     <div className="card bg-base-300 w-96 shadow-xl ">
@@ -87,6 +53,4 @@ const handleLogin = async () => {
   )
 }
 
-export default Login;
-
-
+export default EditProfile
